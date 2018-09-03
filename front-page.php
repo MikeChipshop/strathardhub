50ba27b354be0be4e237fe99ec5606bb81a9b397
MikeChipshop <mike@miniman-webdesign.co.uk>
2018-09-03 07:26:36 +0100

    Various

--- front-page.php
+++ front-page.php
@@ -7,16 +7,61 @@
     </div>
 </div>
 <section class="sbh_fp-facilities">
-    <h1>Hub Facilities</h1>
+    <h1 class="sbh_section-title">Hub Facilities</h1>
     <div class="sbh_fp-facilities-wrap">
         <ul>
             <li>
                 <div class="sbh_fp-facilities-fig">
+                    <i class="fal fa-wifi"></i>
+                </div>
+                <h2>Connected</h2>
+                <div>
+                Sed ut ipsum non dui ornare rutrum eu vel velit. Vivamus luctus est tempus nisi volutpat tempor. In venenatis sit amet risus quis molestie. 
+                </div>
+            </li>
+            <li>
+                <div class="sbh_fp-facilities-fig">
+                <i class="fal fa-comments"></i>
+                </div>
+                <h2>Networking</h2>
+                <div>
+                Sed ut ipsum non dui ornare rutrum eu vel velit. Vivamus luctus est tempus nisi volutpat tempor. In venenatis sit amet risus quis molestie. 
+                </div>
+            </li>
+            <li>
+                <div class="sbh_fp-facilities-fig">
+                    <i class="fal fa-handshake-alt"></i>
+                </div>
+                <h2>Meeting Rooms</h2>
+                <div>
+                Sed ut ipsum non dui ornare rutrum eu vel velit. Vivamus luctus est tempus nisi volutpat tempor. In venenatis sit amet risus quis molestie. 
+                </div>
+            </li>
+            <li>
+                <div class="sbh_fp-facilities-fig">
+                    <i class="fal fa-lock-alt"></i>
+                </div>
+                <h2>Secure</h2>
+                <div>
+                Sed ut ipsum non dui ornare rutrum eu vel velit. Vivamus luctus est tempus nisi volutpat tempor. In venenatis sit amet risus quis molestie. 
+                </div>
+            </li>
+            <li>
+                <div class="sbh_fp-facilities-fig">
+                <i class="fal fa-phone"></i>
+                </div>
+                <h2>Phone</h2>
+                <div>
+                Sed ut ipsum non dui ornare rutrum eu vel velit. Vivamus luctus est tempus nisi volutpat tempor. In venenatis sit amet risus quis molestie. 
+                </div>
+            </li>
+            <li>
+                <div class="sbh_fp-facilities-fig">
                     <i class="fal fa-envelope"></i>
                 </div>
-                <h2>Name of service</h2>
+                <h2>Mail</h2>
                 <div>
-                    Set yourself free and use your time more efficiently by joining Starthard's newest coworking space
+                Sed ut ipsum non dui ornare rutrum eu vel velit. Vivamus luctus est tempus nisi volutpat tempor. In venenatis sit amet risus quis molestie. 
                 </div>
             </li>
         </ul>
