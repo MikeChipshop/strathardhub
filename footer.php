d9b43e3d03ea85a64f06c25bd4a7ea53bb0632bd
Mike Bishop <mike@miniman-webdesign.co.uk>
2018-09-04 12:25:53 +0100

    added no refer to miniman link

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <footer class="sbh_global-footer">
     <div class="sbh_wrap">
         <div class="sbh_footer-copyright">Site and content &copy; <?php echo date("Y"); ?> Strathard Hub</div>
-        <div class="sbh_footer-credit">Web Design and Development by <a href="https://miniman-webdesign.co.uk" target="_blank">Miniman</a></div>
+        <div class="sbh_footer-credit">Web Design and Development by <a href="https://miniman-webdesign.co.uk" target="_blank" rel="noreferrer">Miniman</a></div>
     </div>
 </footer>
 <?php wp_footer(); ?>
