7d7673d29b18f64befc3a68c8290a074da6302f1
Mike Bishop <mike@miniman-webdesign.co.uk>
2018-10-29 09:18:28 +0000

    Various

--- js/site-common.js
+++ js/site-common.js
@@ -1,3 +1,14 @@
+// Register the service worker
+if ('serviceWorker' in navigator) {
+	navigator.serviceWorker.register('https://strathardhub.com/service-worker.js').then(function(registration) {
+    // Registration was successful
+    console.log('ServiceWorker registration successful with scope: ', registration.scope);
+}).catch(function(err) {
+    // registration failed :(
+    	console.log('ServiceWorker registration failed: ', err);
+    });
+}
+
 jQuery(document).ready(function( $ ) {
 
     $(".sbh_fp-hero button").click(function() {
