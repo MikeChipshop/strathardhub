d436b6cc4b39a5bb4fba41bd6df37c955de9957b
Mike Bishop <mike@miniman-webdesign.co.uk>
2018-10-30 08:33:23 +0000

    Work on single articles

---
+++ single.php
@@ -0,0 +1,19 @@
+<?php get_header(); ?>
+    <div class="sbh_wrap">
+        <?php if (have_posts()) : ?>
+            <?php while (have_posts()) : the_post(); ?>
+                <article id="post-<?php the_ID(); ?>" <?php post_class('sbh_main-article'); ?>>
+                    <header>
+                        <h1><?php the_title(); ?></h1>
+                        <h2>Published <time><?php the_time('jS F Y'); ?></time> in <span class="sbh_post-meta-category"><?php the_category( ', ' ); ?></span></h2>
+                    </header>
+                    <div class="sbh_article-content rte">
+                        <?php the_content(); ?>
+                    </div>
+                </article>
+            <?php endwhile; ?>
+        <?php else: ?>
+            <h1>No content</h1>
+        <?php endif; ?>
+    </div>
+<?php get_footer(); ?>
