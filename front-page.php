7ffa8828184ca411db1c282fd68affc99475c034
Mike Bishop <mike@miniman-webdesign.co.uk>
2020-09-15 09:54:07 +0100

    V

--- front-page.php
+++ front-page.php
@@ -163,6 +163,9 @@
         </div>
         <div class="sbh_fp-contact-col-two">
             <?php echo do_shortcode('[contact-form-7 id="72" title="Contact Form"]'); ?>
+            <div class="sbh_google-terms">
+                This site is protected by reCAPTCHA and the Google <a href="https://policies.google.com/privacy" target="_blank" rel="noopener noreferrer">Privacy Policy</a> and <a href="https://policies.google.com/terms" target="_blank" rel="noopener noreferrer">Terms of Service</a> apply.
+            </div>
         </div>
     </div>
 </section>
